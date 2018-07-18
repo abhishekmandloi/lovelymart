fe6565092d37ac2b74d2ea0bdcee02c4f472f4e9
abhishekmandloi <36253595+abhishekmandloi@users.noreply.github.com>
2018-07-18 18:51:39 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,7 @@
 <?php
-/*
-$host="192.168.0.11";
-$user="teachte3_unidb";
-$pass="unidbteach";
-$db_name="teachte3_akc";
-	*/
 	$host="127.0.0.1";
 	$user="root";
 	$pass="";
 	$db_name="my_cloud";
 	$con=new mysqli($host,$user,$pass,$db_name);
-	
-
 ?>
